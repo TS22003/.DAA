cc2a60e49de560dec879021f783bcf4f6a841e51
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-08-01 23:34:05 +0530

    Update and rename 1.c to 1a.c

--- 1a.c
+++ 1a.c
@@ -69,9 +69,5 @@ fprintf(fp,"%d\t%d\t%d\t%d\t%d\n",m,n,gm,gs,gc);
 }
 fclose(fp);
 fclose(fpc);
-FILE *fg = fopen("GCDplot.gnu","w");
-fprintf(fg,"set xlabel \"min{m,n}\"\nset ylabel \"t\"\nplot 'GCDC.txt' u 3 : 4 w l ti\"Ecuclid's\",'GCDC.txt' u 3 : 5 w l ti\"Subtraction\",'GCDC.txt' u 3 : 6 w l ti\"Consecutive Interger\" \nset term png\nset output \"GCD.png\"\nreplot\nset term x11");
-fclose(fg);
-system("gnuplot \"GCDplot.gnu\"");
-system("eog GCD.png");
+
 }
