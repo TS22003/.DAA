e04686493d2ca6ff05b607140e447a46b42fe36d
TS22003 <139066557+TS22003@users.noreply.github.com>
2023-08-02 20:24:04 +0530

    Update 10a.c

--- 10a.c
+++ 10a.c
@@ -1,88 +1,42 @@
-//Test
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+void main()
+{
+    int graph[MAX][MAX], indeg[MAX] = {0}, n, i, j, count = 0, stack[MAX], top = -1, node;
+    printf("\nEnter the number of nodes in the graph:\n");
+    scanf("%d", &n);
+    printf("\nEnter the adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
 
-/*program to implement the topological sorting with dsource removal method on adjacency matrix*/
-#include <stdio.h> 
- #include <stdlib.h> 
- // LEgend 
- int a[100][100]; 
- int in[100], v[100], s[100], t[100]; // 
- int f = 0, r = -1;                   // 
- int n, scount = 0; 
- void computeindegree() 
- { 
-     for (int i = 0; i < n; i++) 
-     { 
-         in[i] = 0; 
-         v[i] = 0; 
-     } 
-     for (int i = 0; i < n; i++) 
-     { 
-         for (int j = 0; j < n; j++) 
-         { 
-             if (a[j][i] == 1) 
-             { 
-                 in[i]++; 
-             } 
-         } 
-         // printf("%d\t%d\n", i, in[i]); 
-         if (in[i] == 0) 
-         { 
-             // printf("\n%d", i); 
-             scount++; 
-             s[++r] = i; 
-             v[i] = 1; 
-         } 
-     } 
- } 
- void topo() 
- { 
-     int cnt = 0; 
-     while (scount != 0) 
-     { 
-         int src = s[f++]; 
-         scount--; 
-         t[cnt] = src; 
-         cnt++; 
-         for (int i = 0; i < n; i++) 
-         { 
-             if (a[src][i] == 1) 
-             { 
-                 in[i]--; 
-             } 
-             if (in[i] == 0 && v[i] == 0) 
-             { 
-                 scount++; 
-                 v[i] = 1; 
-                 s[++r] = i; 
-             } 
-         } 
-     } 
-     if (cnt != n) 
-     { 
-         printf("Cycle Exist"); 
-     } 
-     else 
-     { 
-         printf("\nThe topological sorting is\n"); 
-         for (int i = 0; i < n; i++) 
-             printf("%d\t", t[i] + 1); 
-     } 
- } 
- void main() 
- { 
-     int i, j; 
-     printf("enter the number of vertices:"); 
-     scanf("%d", &n); 
-  
-     printf("Enter the adjacency matrix:\n"); 
-     for (i = 0; i < n; i++) 
-     { 
-         for (j = 0; j < n; j++) 
-         { 
-             scanf("%d", &a[i][j]); 
-         } 
-     } 
-     computeindegree(); 
-     topo(); 
- }
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            indeg[i] += graph[j][i];
 
+    for (i = 0; i < n; i++)
+        if (indeg[i] == 0)
+            stack[++top] = i;
+
+    if(top<0)
+        printf("No source found\n");
+    else{
+    printf("\nThe topological order is:\n");
+    while (top != -1)
+    {
+        node = stack[top--];
+        printf(" => %c", node + 65);
+        for (i = 0; i < n; i++)
+        {
+            if (graph[node][i])
+            {
+                graph[node][i] = 0;
+                indeg[i]--;
+                if (indeg[i] == 0)
+                    stack[++top] = i;
+            }
+        }
+    }
+}
+}
