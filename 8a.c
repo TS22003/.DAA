5605dda14ead4be74a40001db7596b6074acd196
TS22003 <139066557+TS22003@users.noreply.github.com>
2023-08-02 20:22:17 +0530

    Update 8a.c

--- 8a.c
+++ 8a.c
@@ -1,28 +1,31 @@
-//Test
-#include<stdio.h>
-#include<stdlib.h>
-int bfsCount = 0, cyclic=0;
-int count = 0;//to count how many vertex visited 
-int orderCount = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int count = 0, order = 0, cyclic = 0, n;
+int visited[MAX] = {0}, graph[MAX][MAX];
 
-int graph[100][100], visited[100];
-
-void bfs(int n, int start){
-    int queue[n], parent[n];
-    int rear = -1, front = -1, i, parentNode;
-    visited[start] = 1; count++;
-    queue[++rear] = start;
+void bfs(int start)
+{
+    int queue[MAX], parent[MAX], parentnode, rear = 0, front = -1;
+    count++;
+    visited[start] = 1;
+    queue[rear] = start;
     parent[rear] = -1;
-    while(rear != front){
-        start = queue[++front];
-        parentNode = parent[front];
-        printf("-->%c", start+65);
-        for(i=0; i<n; i++){
-            orderCount++;
-            if (i != parentNode && graph[start][i] && visited[i])
+    while (rear != front)
+    {
+        front++;
+        start = queue[front];
+        parentnode = parent[front];
+        printf("--> %d ", start);
+        for (int i = 0; i < n; i++)
+        {
+            order++;
+            if (i != parentnode && graph[start][i] && visited[i])
                 cyclic = 1;
-            if((graph[start][i]) && (visited[i] == 0)){
-                queue[++rear] = i;
+            if (graph[start][i] && !visited[i])
+            {
+                rear++;
+                queue[rear] = i;
                 parent[rear] = start;
                 visited[i] = 1;
                 count++;
@@ -30,51 +33,38 @@ void bfs(int n, int start){
         }
     }
 }
-
-void main(){
-    int n, i, j, start;
-    printf("Enter the number of nodes in the graph:\n");
+void main()
+{
+    int i, j, start;
+    printf("Enter the number of nodes : \n");
     scanf("%d", &n);
-    printf("Enter the Adjacency Matrix:\n");
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
+    printf("Enter the Adjacency Matrix : \n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
             scanf("%d", &graph[i][j]);
-        }
-        visited[i] = 0; 
-    }
-
-     printf("the Adjacency Matrix:\n");
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            printf("%d ", graph[i][j]);
-        }
-        printf("\n");
-    }
-     bfsCount = 0, cyclic=0;
-     count = 0;
-     orderCount = 0;
-    printf("Breadth First Search Traversal:\n");
-    bfsCount++;
-    bfs(n, 0);
-    if(count == n){
-        printf("\nGraph is connected.\n");
-    }
-    else {
-        printf("\nThe graph is not connected.\n");
-        start = 1;
-        while(count != n){
-            if(visited[start] != 1) {
-                bfsCount++;
-                bfs(n, start);
-                printf("\n");
+    printf("Enter the starting vertex : \n");
+    scanf("%d", &start);
+    printf("\nBreadth First Search Traversal: \nStarting Node: %d\n", start);
+    bfs(start);
+    if (count == n)
+        printf("\nGraph is connected");
+    else
+    {
+        printf("\nThe graph is not connected");
+        start = 0;
+        while (count != n)
+        {
+            if (visited[start] != 1)
+            {
+                printf("\nStarting Node: : %d\n", start);
+                bfs(start);
             }
             start++;
         }
     }
-    printf("\nThe number of components in the graph is %d\n", bfsCount);
-    if(cyclic) {
-        printf("\nThe graph is cyclic\n");
-    } else {
-        printf("\nThe graph is acyclic\n");
-    }
+    if (cyclic == 0)
+        printf("\nGraph is Acyclic\n");
+    else
+        printf("\nGraph is Cyclic\n");
+    printf("Order of count : %d\n", order);
 }
